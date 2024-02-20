ea6e895ccf3a821d93f35db14923bb5986d193ed
UndurthiCharanTeja <98147960+UndurthiCharanTeja@users.noreply.github.com>
2024-02-20 20:51:21 +0530

    import java.util.*; public class ashii{     public static void main(String args[]){         Scanner ip =new Scanner(System.in);         Double n1,n2,res;         n1=ip.nextDouble();         n2=ip.nextDouble();         res=n1*n2;         System.out.format("%.2f",res);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ashii{
+    public static void main(String args[]){
+        Scanner ip =new Scanner(System.in);
+        Double n1,n2,res;
+        n1=ip.nextDouble();
+        n2=ip.nextDouble();
+        res=n1*n2;
+        System.out.format("%.2f",res);
+    }
+}
